8dd49c3d062ece841f51fda4b30f79eba2e823a0
José Sousa <zpedro_s@hotmail.com>
2023-07-18 14:04:39 +0100

    add stryker config

---
+++ mutation-tests/Calculator/VatCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace mutation_tests
+{
+    public static class VatCalculator
+    {
+        public static double CalculateFinalPrice(double productPrice)
+        {
+            if (productPrice < 200)
+            {
+                return productPrice + (productPrice * 0.23);
+            }
+
+            return productPrice;
+        }
+    }
+}
---
+++ mutation-tests/Tests.Calculator/VatCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿namespace medium_mutation_tests
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using mutation_tests;
+
+    [TestClass]
+    public class VatCalculatorTests
+    {
+        [TestMethod]
+        public void VatCalculator_ProductMoreThan200_VatFree()
+        {
+            const double productValue = 485;
+
+            var finalPrice = VatCalculator.CalculateFinalPrice(productPrice: productValue);
+
+            productValue.Should().Be(finalPrice);
+        }
+        
+        [TestMethod]
+        public void VatCalculator_ProductLessThan200_VatFree()
+        {
+            const double productValue = 8.8;
+
+            var finalPrice = VatCalculator.CalculateFinalPrice(productPrice: productValue);
+
+            finalPrice.Should().Be((productValue + (productValue * 0.23)));
+        }
+    }
+}
